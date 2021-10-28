c180028754de7097cf17909c7304bd35a4f8d95b
levietanh_bi9035 <anhlv.bi9035@st.usth.edu.vn>
2021-10-28 20:41:54 +0700

    create venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Python\django\drugs_and_diseases_management_system\venv
+set VIRTUAL_ENV=D:\Python\django\ddms7\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
