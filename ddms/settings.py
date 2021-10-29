0a109c5bcae6e01fd50ff6b2a6c0096589ab04dd
levietanh_bi9035 <anhlv.bi9035@st.usth.edu.vn>
2021-10-29 08:12:20 +0700

    fix bug

--- ddms/settings.py
+++ ddms/settings.py
@@ -169,7 +169,7 @@ WSGI_APPLICATION = 'ddms.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
         
         'ATOMIC_REQUESTS': True,
     }
